bdce552d1e7d87cab2d3a6e7ab860bc5bd7b00fd
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-21 02:14:42 +0300

    [core] Added repository `RabbitRepository`

---
+++ src/repositories/rabbit.py
@@ -0,0 +1,28 @@
+import json
+from typing import Protocol
+from typing import Self
+
+from aiogram.types import Message
+
+from src.infra.rabbit import Rabbit
+
+
+class RabbitRepositoryProtocol(Protocol):
+  async def get_user_stats(self: Self, message: Message) -> None:
+    ...
+
+
+class RabbitRepositoryImpl:
+  async def get_user_stats(self: Self, message: Message) -> None:
+    """Publishes a message to send a user stats from the cache
+    """
+
+    await Rabbit.broker.publish(
+      message=json.dumps({
+        "user_tg_id": message.from_user.id,
+      }),
+      routing_key="user_stats",
+    )
+
+
+RabbitRepository = RabbitRepositoryImpl()
