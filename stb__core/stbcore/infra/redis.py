5a71cd9a919bf25805407a88dc38f6c8e0ca58d3
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-22 14:23:03 +0300

    [stbcore] Updated infra `redis`

--- stb__core/stbcore/infra/redis.py
+++ stb__core/stbcore/infra/redis.py
@@ -1,17 +1,24 @@
 from typing import Self
 
 from faststream.redis import RedisBroker
+import redis
 
 from stbcore.core.config import settings
 
 
 class Redis:
 	broker: RedisBroker | None = None
+	client: redis.Redis | None = None
 
 	@classmethod
 	async def initialize(cls: Self) -> None:
 		cls.broker = RedisBroker(url=settings.redis.broker_url)
 
 	@classmethod
+	async def initialize_client(cls: Self) -> None:
+		cls.client = cls.broker.connect()
+
+	@classmethod
 	async def terminate(cls: Self) -> None:
+		await cls.client.close()
 		await cls.broker.stop()
