4908943b325cee742829855231676af888b40528
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-22 16:01:15 +0300

    [core] Initialized infra `minio`

--- main.py
+++ main.py
@@ -11,28 +11,37 @@ if not os.environ.get("IS_IN_PRODUCTION_MODE"):
 	load_dotenv(find_dotenv())
 
 from stbcore.infra.kafka import Kafka
-from stbcore.infra.postgres import Postgres
 from stbcore.infra.rabbit import Rabbit
+from stbcore.infra.postgres import Postgres
+from stbcore.infra.minio import Minio
 
 from src.app import bot, dp
 from src.routes import routers
 
 
 async def initialize():
+	# Async
 	await Kafka.initialize()
 	await Rabbit.initialize()
 	await Postgres.initialize()
 
+	# Sync
+	Minio.initialize()
+
+	# Routers
 	dp.include_routers(
 		*routers
 	)
 
 
 async def terminate():
+	# Async
 	await Kafka.terminate()
 	await Rabbit.terminate()
 	await Postgres.terminate()
 
+	# Sync
+	Minio.terminate()
 
 async def main():
 	await dp.start_polling(bot)
