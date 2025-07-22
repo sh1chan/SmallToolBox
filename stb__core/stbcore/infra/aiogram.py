4f4e54ce1068e99b1ab5425f4d6932d6b5aa0c18
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-22 16:14:58 +0300

    [stbcore] Added infra `aiogram`

---
+++ stb__core/stbcore/infra/aiogram.py
@@ -0,0 +1,22 @@
+from typing import Self
+
+from aiogram import Bot
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
+from stbcore.core.config import settings
+
+
+class Aiogram:
+	bot: Bot | None = None
+
+	@classmethod
+	def initialize(cls: Self) -> None:
+		cls.bot = Bot(
+			token=settings.tgbot.token,
+			default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+		)
+
+	@classmethod
+	def terminate(cls: Self) -> None:
+		cls.bot.close()
