25daaa69cbb70d5e90e8bc6300f2d5c6f37dc4d1
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-27 00:55:37 +0300

    [core] Updated repositories `RootRepository`

--- src/repositories/root.py
+++ src/repositories/root.py
@@ -3,7 +3,10 @@ from typing import Self
 
 from aiogram.types import Message
 
-from stbcore.schemas.kafka import MessageEventSchema
+from stbcore.schemas.kafka import TelegramUserObjectSchema
+from stbcore.schemas.kafka import TelegramChatObjectSchema
+from stbcore.schemas.kafka import TelegramMessageObjectSchema
+from stbcore.schemas.kafka import TelegramMessageEventSchema
 
 from .kafka import KafkaRepository
 from .kafka import KafkaRepositoryProtocol
@@ -16,7 +19,7 @@ class RootRepositoryProtocol(Protocol):
 	def __init__(
 			self: Self,
 			kafka_repository: KafkaRepositoryProtocol,
-	):	...
+	) -> None:	...
 
 	async def register_message(self: Self, message: Message) -> None:	...
 
@@ -28,21 +31,35 @@ class RootRepositoryImpl:
 	def __init__(
 			self: Self,
 			kafka_repository: KafkaRepositoryProtocol
-	):
+	) -> None:
 		self.kafka_repository = kafka_repository
 
 	async def register_message(self: Self, message: Message) -> None:
+		"""
+		"""
 		await self.kafka_repository.publish_event(
-			payload=MessageEventSchema(
-				user_tg_id=message.from_user.id,
-				chat_tg_id=message.chat.id,
-				message_tg_id=message.message_id,
-				date=message.date.strftime("%Y-%m-%d %H:%M:%S"),
+			payload=TelegramMessageEventSchema(
+				user=TelegramUserObjectSchema(
+					tg_id=message.from_user.id,
+					username=message.from_user.username,
+					full_name=message.from_user.full_name,
+				),
+				chat=TelegramChatObjectSchema(
+					tg_id=message.chat.id,
+					username=message.chat.username,
+					full_name=message.chat.full_name,
+				),
+				message=TelegramMessageObjectSchema(
+					tg_id=message.message_id,
+					date=message.date.strftime("%Y-%m-%d %H:%M:%S"),
+				),
 			)
 		)
 
 
 def get_root_repository() -> RootRepositoryProtocol:
+	"""
+	"""
 	return RootRepositoryImpl(
 		kafka_repository=KafkaRepository,
 	)
