4f4f1bac44ebc4ffdbe5b47d10e981361bef42c3
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-07-30 05:30:17 +0300

    [stb__core] Updated enums, schemas

--- stb__core/stbcore/core/enums.py
+++ stb__core/stbcore/core/enums.py
@@ -9,7 +9,9 @@ class KafkaTopicsEnum(StrEnum):
 
 class RabbitRoutingKeysEnum(StrEnum):
 	STATS_GENERATOR__USER_STATS = auto()
+	STATS_GENERATOR__CHAT_STATS = auto()
 	TG_MESSAGES__USER_STATS = auto()
+	TG_MESSAGES__CHAT_STATS = auto()
 
 
 class MinioBucketsEmum(StrEnum):
--- stb__core/stbcore/schemas/rabbit.py
+++ stb__core/stbcore/schemas/rabbit.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from pydantic import PositiveInt
+from pydantic import NegativeInt
 
 
-class GenerateUserStatsInSchema(BaseModel):
+class GenerateStatsSchema(BaseModel):
+	chat_tg_id: NegativeInt
 	user_tg_id: PositiveInt
 	message_tg_id: PositiveInt
